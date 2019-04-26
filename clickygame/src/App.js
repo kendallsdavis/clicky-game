65bec32cd3b7f242464729243f424339d497a0d2
Kendall Davis <kendallsdavis@gmail.com>
2019-04-26 19:54:44 -0400

    folder set up and initial app.js work

---
+++ clickygame/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const name = "Ahmed";
+const num1 = 1;
+const num2 = 2;
+
+// function App() {
+//   return(
+//     <div className="card">
+//   <img src="..." class="card-img-top" alt="..."></img>
+//   )
+// }
+
+
+function App() {
+  return ( 
+    <div className="card">hello</div>
+
+    // <div className="main-container">
+    //   <div className="container">
+    //     <div className="jumbotron">
+    //       {/* JavaScript expressions can be escaped inside of curly braces */}
+
+    //       <h2>My name is {name}. But you can call me...</h2>
+    //       <h1>The JSX Boss!</h1>
+    //       <hr />
+    //       <h2>I can do math: {num1 + num2}.</h2>
+    //       <h2>
+    //         I can generate random numbers:
+    //         {Math.floor(Math.random() * 10) + 1},{Math.floor(Math.random() * 10) + 1},
+    //         {Math.floor(Math.random() * 10) + 1}.
+    //       </h2>
+    //       <h2>I can even reverse my name: {name.split("").reverse()}</h2>
+    //     </div>
+    //   </div>
+    // </div>
+  )
+}
+
+export default App;
