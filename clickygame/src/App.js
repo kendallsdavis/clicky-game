f07cb4caa7d96fbb8b512190746718c749675528
Kendall Davis <kendallsdavis@gmail.com>
2019-04-26 22:59:22 -0400

    working score increment in console log

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -15,8 +15,24 @@ class App extends Component {
     guessed: []
   };
 
-// this function will be called each time a cat image is clicked
+  
+
+// this function will be called each time a cat image is clicked. The click event passes the id of the cat selected 
+// into the function
 shuffleCats = (id) => {
+  
+  // check and see if the cat guessed has already been guessed. If it has, reset the array of guesses to empty and the score to 0
+  if(this.state.guessed.includes(id)){
+    console.log(id)
+    console.log(this.state.guessed)
+    console.log("Game Over!");
+    this.setState({
+    guessed:[],
+    score: 0
+    })
+  } else {
+    // if it hasn't, push the photo id into the array of guesses and shuffle the cats
+    this.state.guessed.push(id)
   let reshuffle = this.state.cats
   for (let i = reshuffle.length - 1; i > 0; i--) {
         const randomIndex = Math.floor(Math.random() * (i + 1));
@@ -25,11 +41,14 @@ shuffleCats = (id) => {
         reshuffle[i] = itemAtIndex;
       }
       console.log(id)
-      this.state.guessed.push(id)
+      
       console.log(this.state.guessed)
   this.setState({
-    cats: reshuffle
+    cats: reshuffle,
+    score: this.state.score+1
   })
+  console.log(this.state)
+}
 };
 
 
