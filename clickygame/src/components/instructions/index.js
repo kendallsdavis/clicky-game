21ae92c17ce78238f6ed9b17cca4a6cb7d7b5dd7
Kendall Davis <kendallsdavis@gmail.com>
2019-04-28 15:47:07 -0400

    ready to deploy

--- clickygame/src/components/instructions/index.js
+++ clickygame/src/components/instructions/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Instructions(props) {
-  return <span id="instructions" className="instructions">Test your memory and click through each fat cat imagine without any repeats! Every time you click the images will re-shuffle. Try to reach a perfect score of 9!</span>;
+  return <span id="instructions" className="instructions">Test your memory and click through each fat cat image without any repeats! Every time you click the images will re-shuffle. Try to reach a perfect score of 9!</span>;
 }
 
 export default Instructions;
--- clickygame/src/components/score/index.js
+++ clickygame/src/components/score/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Score(props) {
-  return <span className="score" id="score">Current Score: {props.children}{props.score}</span>;
+  return <span className="animated heartBeat" id="score">Current Score: {props.children}{props.score}</span>;
 }
 
 export default Score;
