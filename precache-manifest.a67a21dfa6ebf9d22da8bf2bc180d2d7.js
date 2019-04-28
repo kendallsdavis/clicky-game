9be130c34d21f135ae5e8d6c5c729bec2ffa280f
Kendall Davis <kendallsdavis@gmail.com>
2019-04-28 16:01:26 -0400

    Updates

---
+++ precache-manifest.a67a21dfa6ebf9d22da8bf2bc180d2d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce203000a858063397b8814df07e96e2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "febeb45eca651adf37e3",
+    "url": "/clicky-game/static/css/main.5f25ad88.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "febeb45eca651adf37e3",
+    "url": "/clicky-game/static/js/main.34a034bb.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
